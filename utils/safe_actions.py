af29d304825f45f4a630c523c39d520e7c5d3239
meghana <meghanadwasari@gmail.com>
2026-05-08 13:38:23 +0530

    Remove safe_actions.py from utils

--- utils/safe_actions.py
+++
@@ -1,7 +0,0 @@
-
-from tenacity import retry, stop_after_attempt, wait_fixed
-from selenium.common.exceptions import WebDriverException
-
-@retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
-def safe_get(driver, url):
-    driver.get(url)
